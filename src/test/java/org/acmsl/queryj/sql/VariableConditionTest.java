5a06480eab522f15c88cdc05f3b087fa01a42e5a
chous <chous@acm-sl.org>
2014-07-31 18:57:16 +0200

    Moved queryj-sql from old git repository

---
+++ src/test/java/org/acmsl/queryj/sql/VariableConditionTest.java
@@ -0,0 +1,241 @@
+/*
+                        QueryJ
+
+    Copyright (C) 2002-2005  Jose San Leandro Armendariz
+                        chous@acm-sl.org
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    General Public License for more details.
+
+    You should have received a copy of the GNU General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+    Thanks to ACM S.L. for distributing this library under the GPL license.
+    Contact info: chous@acm-sl.org
+    Postal Address: c/Playa de Lagoa, 1
+                    Urb. Valdecabanas
+                    Boadilla del monte
+                    28660 Madrid
+                    Spain
+
+ ******************************************************************************
+ *
+ * Filename: $RCSfile$
+ *
+ * Author: Jose San Leandro Armendariz
+ *
+ * Description: Indicates JUnit how to test Condition classes.
+ *
+ */
+package org.acmsl.queryj.sql;
+
+/*
+ * Importing JDK classes.
+ */
+import java.util.Arrays;
+import java.util.Collection;
+
+/*
+ * Importing JUnit classes.
+ */
+import junit.framework.TestCase;
+
+/*
+ * Importing JetBrains annotations.
+ */
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Indicates JUnit how to test VariableCondition classes.
+ * @author <a href="mailto:chous@acm-sl.org">Jose San Leandro</a>
+ */
+public class VariableConditionTest
+    extends  TestCase
+{
+    /**
+     * The expected condition.
+     */
+    protected static final String EXPECTED_CONDITION =
+        "(USERS.USERID > 10) AND ((USERS.NAME = ?) OR (USERS.NAME is null)) OR (USERS.NAME = ?)";
+
+    /**
+     * The USERS table.
+     */
+    protected static final UsersTable USERS = new UsersTable() {};
+
+    /**
+     * Constructs a test case with the given name.
+     * @param name the test case name.
+     */
+    public VariableConditionTest(@NotNull final String name)
+    {
+        super(name);
+    }
+
+    /**
+     * Tests the condition class.
+     * @see org.acmsl.queryj.sql.Condition
+     */
+    @SuppressWarnings("unused")
+    public void test1Condition()
+    {
+        @NotNull final Condition t_Condition = USERS.USERID.greaterThan();
+
+        final Collection<VariableCondition> t_cVariableConditions = t_Condition.getVariableConditions();
+
+        assertTrue(true);
+
+        assertTrue(t_cVariableConditions.size() == 1);
+    }
+
+    /**
+     * Tests the condition class.
+     * @see org.acmsl.queryj.sql.Condition
+     */
+    @SuppressWarnings("unused")
+    public void test2Condition()
+    {
+        @NotNull final Condition t_Condition =
+            USERS.USERID.greaterThan().and(USERS.USERID.equals());
+
+        @NotNull final Collection<VariableCondition> t_cVariableConditions = t_Condition.getVariableConditions();
+
+        assertTrue(t_cVariableConditions.size() == 2);
+    }
+
+    /**
+     * Tests the condition class.
+     * @see org.acmsl.queryj.sql.Condition
+     */
+    @SuppressWarnings("unused")
+    public void test3Condition()
+    {
+        @NotNull final Condition t_Condition =
+            USERS.USERID.greaterThan(1).and(USERS.USERID.equals()).and(USERS.USERID.equals(1));
+
+        @NotNull final Collection<VariableCondition> t_cVariableConditions = t_Condition.getVariableConditions();
+
+        assertTrue(t_cVariableConditions.size() == 1);
+    }
+
+    /**
+     * Tests the condition class.
+     * @see org.acmsl.queryj.sql.Condition
+     */
+    @SuppressWarnings("unused")
+    public void test3Condition2()
+    {
+        @NotNull final Condition t_Condition =
+            USERS.USERID.greaterThan(1).and(USERS.USERID.equals()).and(USERS.USERID.equals());
+
+        @NotNull final Collection<VariableCondition> t_cVariableConditions = t_Condition.getVariableConditions();
+
+        assertTrue(t_cVariableConditions.size() == 2);
+    }
+
+    /**
+     * Tests the condition class.
+     * @see org.acmsl.queryj.sql.Condition
+     */
+    @SuppressWarnings("unused")
+    public void test3Condition3()
+    {
+        @NotNull final Condition t_Condition =
+            USERS.USERID.greaterThan().and(USERS.USERID.equals()).and(USERS.USERID.equals());
+
+        @NotNull final Collection<VariableCondition> t_cVariableConditions = t_Condition.getVariableConditions();
+
+        assertTrue(t_cVariableConditions.size() == 3);
+    }
+
+    /**
+     * Executes the tests from command line.
+     * @param args the command-line arguments. Not needed so far.
+     */
+    public static void main(@NotNull final String[] args)
+    {
+        junit.textui.TestRunner.run(VariableConditionTest.class);
+    }
+
+    /**
+     * Test-only table.
+     * @author <a href="mailto:chous@acm-sl.org">Jose San Leandro</a>
+     * @version $Revision$
+     */
+    public static class UsersTable
+        extends  Table
+    {
+        /**
+         * The USERS table USERID field.
+         */
+        @NotNull
+        public IntField USERID =
+            new IntField("USERID", this) {};
+
+        /**
+         * The USERS table NAME field.
+         */
+        @NotNull
+        public Field NAME =
+            new Field("NAME", this) {};
+
+        /**
+         * All fields.
+         */
+        @NotNull
+        public Field[] ALL =
+            new Field[] {USERID, NAME};
+
+        /**
+         * The table name.
+         */
+        public static final String TABLE_NAME = "USERS";
+
+        /**
+         * The table alias.
+         */
+        @NotNull
+        public TableAlias ALIAS =
+            new TableAlias("usrs", this) {};
+
+        /**
+         * Creates a USERS table.
+         */
+        protected UsersTable()
+        {
+            super(TABLE_NAME);
+        }
+
+        /**
+         * Retrieves <code>all</code> fields. It's equivalent to a
+         * star in a query.
+         * @return such fields.
+         */
+        @NotNull
+        @Override
+        public Field[] getAll()
+        {
+            return ALL;
+        }
+
+        @Override
+        public String toString()
+        {
+            return "UsersTable{" +
+                   "ALIAS=" + ALIAS +
+                   ", USERID=" + USERID +
+                   ", NAME=" + NAME +
+                   ", ALL=" + Arrays.toString(ALL) +
+                   '}';
+        }
+    }
+}
+
